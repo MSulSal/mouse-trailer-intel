57bc7c86f59412010c4d431adf35c761aa07813c
MSulSal <msulsaleem@gmail.com>
2025-04-23 18:38:48 -0500

    try through js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const trailer = document.getElementById("trailer");
 
-const getTrailerClass = (type) => {
-  switch (type) {
-    case "video":
-      return "fa-solid fa-play";
-    default:
-      return "fa-solid fa-arrow-up-right-from-square";
-  }
-};
-
 const animateTrailer = (e, interacting) => {
   const x = e.clientX - trailer.offsetWidth / 2,
     y = e.clientY - trailer.offsetHeight / 2;
 
-  trailer.style.transform = `translate(${x}px, ${y}px)`;
-
   const keyframes = {
-    transform: `translate(${x}px, ${y}px)`,
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`,
   };
 
   trailer.animate(keyframes, {
     duration: 800,
     fill: "forwards",
   });
+};
+
+const getTrailerClass = (type) => {
+  switch (type) {
+    case "video":
+      return "fa-solid fa-play";
+    default:
+      return "fa-solid fa-arrow-up-right";
+  }
 };
 
 window.onmousemove = (e) => {
@@ -37,7 +35,5 @@ window.onmousemove = (e) => {
 
   if (interacting) {
     icon.className = getTrailerClass(interactable.dataset.type);
-  } else {
-    icon.className = "";
   }
 };
